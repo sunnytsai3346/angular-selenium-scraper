72edc27f3e09bfd149675c236d7f9f8b3a325476
Sunny Tsai <pl.sunnytsai@gmail.com>
2025-07-31 05:59:10 -0400

    upate fille_en_json

--- fill_en_json.py
+++ fill_en_json.py
@@ -1,6 +1,6 @@
 import json
 import os
-from urllib.parse import urljoin, urlparse, urlunparse, uses_netloc, uses_relative
+from urllib.parse import urljoin
 
 # The base URL for constructing the full URL
 BASE_URL = "http://192.168.230.169/"
@@ -47,7 +47,8 @@ def merge_json_data(en_json_path='context/EN.json', output_path='en_filled.json'
         elif 'value' not in en_item:
             en_item['value'] = None
         en_url = en_item.get('url')    
-        en_item['url'] = urljoin(BASE_URL, en_url)
+        if en_url:
+            en_item['url'] = urljoin(BASE_URL, en_url)
 
     # Write the updated data to a new file
     try:
@@ -56,74 +57,6 @@ def merge_json_data(en_json_path='context/EN.json', output_path='en_filled.json'
         print(f"Successfully merged data and created {output_path}.")
     except IOError as e:
         print(f"Error writing to {output_path}: {e}")
-
-def urljoin(base, url, allow_fragments=True):
-    """Join a base URL and a possibly relative URL to form an absolute
-    interpretation of the latter."""
-    if not base:
-        return url
-    if not url:
-        return base
-
-    base, url, _coerce_result = _coerce_args(base, url)
-    bscheme, bnetloc, bpath, bparams, bquery, bfragment = \
-            urlparse(base, '', allow_fragments)
-    scheme, netloc, path, params, query, fragment = \
-            urlparse(url, bscheme, allow_fragments)
-
-    if scheme != bscheme or scheme not in uses_relative:
-        return _coerce_result(url)
-    if scheme in uses_netloc:
-        if netloc:
-            return _coerce_result(urlunparse((scheme, netloc, path,
-                                              params, query, fragment)))
-        netloc = bnetloc
-
-    if not path and not params:
-        path = bpath
-        params = bparams
-        if not query:
-            query = bquery
-        return _coerce_result(urlunparse((scheme, netloc, path,
-                                          params, query, fragment)))
-
-    base_parts = bpath.split('/')
-    if base_parts[-1] != '':
-        # the last item is not a directory, so will not be taken into account
-        # in resolving the relative path
-        del base_parts[-1]
-
-    # for rfc3986, ignore all base path should the first character be root.
-    if path[:1] == '/':
-        segments = path.split('/')
-    else:
-        segments = base_parts + path.split('/')
-        # filter out elements that would cause redundant slashes on re-joining
-        # the resolved_path
-        segments[1:-1] = filter(None, segments[1:-1])
-
-    resolved_path = []
-
-    for seg in segments:
-        if seg == '..':
-            try:
-                resolved_path.pop()
-            except IndexError:
-                # ignore any .. segments that would otherwise cause an IndexError
-                # when popped from resolved_path if resolving for rfc3986
-                pass
-        elif seg == '.':
-            continue
-        else:
-            resolved_path.append(seg)
-
-    if segments[-1] in ('.', '..'):
-        # do some post-processing here. if the last segment was a relative dir,
-        # then we need to append the trailing '/'
-        resolved_path.append('')
-
-    return _coerce_result(urlunparse((scheme, netloc, '/'.join(
-        resolved_path) or '/', params, query, fragment)))
 
 def main():
     """
